ef1371874f3749cbb7c3aeaacc69737182314526
lisacyc <melissa111117@gmail.com>
2022-08-17 20:08:56 +0800

    商品功能 - 設定返回的時間格式和時區

--- src/main/java/com/chenlisa/springbootmall/controller/ProductController.java
+++ src/main/java/com/chenlisa/springbootmall/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.chenlisa.springbootmall.controller;
 
-import com.chenlisa.springbootmall.constant.ProductCategory;
 import com.chenlisa.springbootmall.model.Product;
 import com.chenlisa.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
 
 @RestController
 public class ProductController {
